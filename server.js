5fb21307264e4df61a09963754cbca138c043dd9
samet <astopaal@gmail.com>
2022-11-10 16:45:00 +0300

    saving question and answers to database

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const webconfig = {
     user: "sa",
     password: "123456",
     database: "survey",
-    server: 'DESKTOP-RKVVLOQ',
+    server: 'ABS-DEV',
     pool: {
         max: 10,
         min: 0,
@@ -26,7 +26,7 @@ const webconfig = {
     }
 }
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
 app.use(bodyParser.json());
 /*run this server in 8080*/
@@ -37,11 +37,35 @@ app.get('/', cors(), async (req, res) => {
 
 let questionsData = [];
 
+//oluşturulan anket verisini post eder
 app.post("/question_data", cors(), async (req, res) => {
     questionsData = req.body;
-    console.log(questionsData);
-    res.send(JSON.stringify(questionsData));
-});
+    res.send(JSON.stringify((questionsData)));
+    let pool = await sql.connect(webconfig);
+    let guid = String(Date.now()) ;
+    for (let i = 0; i < questionsData.questionsData.length; i++) {
+        console.log(questionsData.questionsData[i].question);
+        
+        try {
+            let result = await pool.request()
+            .query(`INSERT INTO TBLSorular (Guid, TextSoru, Tip, IsActive, IsDeleted) values ('${guid}', '${questionsData.questionsData[i].question}', 'Çoktan Seçmeli', 1, 0)`);
+        }
+        catch (err) {
+            console.log(err);
+        }
+        
+        for (let j = 0; j < questionsData.questionsData[i].answers.length; j++) {
+            try {
+                let result = await pool.request()
+                .query(`INSERT INTO TBLCevaplar (SoruID, TextCevap, IsActive, IsDeleted) values ('${questionsData.questionsData[i].answers[j].qid}', '${questionsData.questionsData[i].answers[j].answer}', 1, 0)`);
+            }
+            catch (err) {
+                console.log(err);
+            }
+        }
+    }
+}
+);
 
 app.get('/question_data', cors(), async (req, res) => {
     res.send(JSON.stringify(questionsData));
@@ -51,22 +75,22 @@ let adminData = "";
 
 app.post('/add_admin', cors(), async (req, res) => {
 
-        adminData = (req.body);
-        let un = (adminData.username)
-        let pw = (adminData.password)
-        let mail = (adminData.email)
+    adminData = (req.body);
+    let un = (adminData.username)
+    let pw = (adminData.password)
+    let mail = (adminData.email)
 
-        /*un ve pw değerlerini veritabanında TBLUsers tablosuna ekle*/
-        try {
-            let pool = await sql.connect(webconfig)
-            let result = await pool.request()
-                .query(`INSERT INTO TBLUsers (Username, Password, Eposta) VALUES ('${un}', '${pw}', '${mail}')`)
-            // console.log(result)
-        }
-        catch (err) {
-            // console.log(err)
-        }
+    /*un ve pw değerlerini veritabanında TBLUsers tablosuna ekle*/
+    try {
+        let pool = await sql.connect(webconfig)
+        let result = await pool.request()
+            .query(`INSERT INTO TBLUsers (Username, Password, Eposta) VALUES ('${un}', '${pw}', '${mail}')`)
+        // console.log(result)
     }
+    catch (err) {
+        console.log(err)
+    }
+}
 );
 
 app.get('/add_admin', cors(), async (req, res) => {
@@ -82,14 +106,14 @@ app.post('/admin-login', cors(), async (req, res) => {
     // res.send('success');
     try {
         let pool = await sql.connect(webconfig)
-        await pool.query(`SELECT * FROM TBLUsers WHERE Username = N'${un}' AND Password = N'${pw}'`,(err,data)=>{
-            if(err){
+        await pool.query(`SELECT * FROM TBLUsers WHERE Username = N'${un}' AND Password = N'${pw}'`, (err, data) => {
+            if (err) {
                 throw err;
             }
             // console.log(data.recordset.length)
-            if(data.recordset.length>0){
+            if (data.recordset.length > 0) {
                 res.send('success')
-            }else{
+            } else {
                 res.send('error')
             }
         })
@@ -104,6 +128,6 @@ app.get('/admin-login', cors(), async (req, res) => {
 })
 
 server.listen(8080, function () {
-        console.log('Server listening at port %d', 8080);
-    }
+    console.log('Server listening at port %d', 8080);
+}
 );
